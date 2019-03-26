ca092bb76e208a11f1146bf9cf95532073477c3b
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-26 20:08:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planateer_calls)
-  planateer_calls.map {
+  planateer_calls.map { |call| 
 end
 
 def long_planeteer_calls# code an argument here
